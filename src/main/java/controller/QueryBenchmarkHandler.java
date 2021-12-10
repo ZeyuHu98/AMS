b5cf487afc6a7b69946ad137cab5774e6a2b9418
mynameisfanxuhui <535192593@qq.com>
2021-12-10 14:24:47 -0500

    add query benchmark

---
+++ src/main/java/controller/QueryBenchmarkHandler.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import services.impl.BenchmarkImpl;
+import system.tools.Tools;
+
+public class QueryBenchmarkHandler implements RequestHandler<Map<String, Object>, Map<String, Object>>
+{
+	
+	BenchmarkImpl impl = new BenchmarkImpl();
+	
+	@Override
+	public Map<String, Object> handleRequest(Map<String, Object> request, Context context)
+	{
+		List<Map<String, String>> dataList = new ArrayList<>();
+		int statusCode = 200;
+		try
+		{
+			dataList = impl.query();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+			statusCode = 400;
+		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("statusCode", statusCode);
+		map.put("dataList", Tools.toJson(dataList));
+		
+		return map;
+	}
+}
+
